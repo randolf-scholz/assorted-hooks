6860632cc66c9a62930a2f55e7172bef53b491bd
Randolf Scholz <randolf.scholz@gmail.com>
2024-08-02 14:58:05 +0200

    patched pyright wrapper

--- src/pyright_wrapper.sh
+++ src/pyright_wrapper.sh
@@ -6,10 +6,12 @@ PATTERN="(?<=$PWD/)(.*:.*)" # the pattern to match the error message
 
 # NOTE: script -c preserves the colors of the output
 result=$(script -c "pyright $*" /dev/null | grep --color=never -Po "$PATTERN");
-count=$(echo -n "$result" | wc -l);
+# NOTE: wc -l gives incorrect count if no matches.
+# REF: https://stackoverflow.com/a/42399738
+count=$(echo -n "$result" | grep -c "^");
 
 if [ "$count" -ne 0 ]; then
   echo "$result";
-  echo "${BOLD_RED}Found $((count+1)) errors${RESET}";
+  echo "${BOLD_RED}Found ${count} errors${RESET}";
   exit 1;
 fi
