b1e24c4ab9b40e6b0a1b3aa1a8dd45a4ee30991e
Randolf Scholz <randolf.scholz@gmail.com>
2025-03-14 20:19:35 +0100

    updated version

--- src/assorted_hooks/__init__.py
+++ src/assorted_hooks/__init__.py
@@ -1,6 +1,8 @@
 r"""Assorted Hooks."""
 
 __all__ = [
+    # constants
+    "__version__",
     # module
     "check_archived_hooks",
     "check_github_issues",
@@ -9,9 +11,18 @@ __all__ = [
 ]
 
 
+from importlib import metadata
+
 from assorted_hooks import (
     check_archived_hooks,
     check_github_issues,
     check_requirements_maintained,
     check_requirements_valid,
 )
+
+try:  # single-source version
+    __version__ = metadata.version(__package__ or __name__)
+except metadata.PackageNotFoundError:
+    __version__ = "0"
+
+del metadata
